3cd171eee8b1e7ff12da7882375d3e679b7e76d0
Cai Jones <1808209+Riott@users.noreply.github.com>
2023-03-31 18:40:25 +0100

    revert webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ require('webpack');
  * @param {http.ClientRequest} request
  */
 function bakeCookies(request) {
-  let cookies = 'sid=';
+  let cookies = request.getHeader('Cookie');
 
   const { SID, REMEMBERME } = process.env;
   if (SID) {
